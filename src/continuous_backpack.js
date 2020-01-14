84606575a6a765c8cd3b4e92659b32313e5b03e7
Ivan Skovpin <ivanskovpin@gmail.com>
2020-01-14 22:55:26 +0700

    + continuous_backpack algorithm

---
+++ src/continuous_backpack.js
@@ -0,0 +1,53 @@
+/*
+ * Task: find the maximum cost of things which can be placed into a backpack. Things could be cut.
+ * Use a greedy algorithm.
+ * See: https://stepik.org/lesson/13238/step/10
+ */
+
+/**
+ * @param backpackVolume {number}
+ * @param things {Array<Array>} - [[cost1, volume2], [cost2, volume2], ...]
+ * @returns {number}
+ */
+function calculateMaxCost(backpackVolume, things) {
+    things.sort(function (a, b) {
+        return b[0]/b[1] - a[0]/a[1];
+    });
+
+    let cost = 0;
+    let i = 0;
+    while (things[i][1] <= backpackVolume) {
+        cost += things[i][0];
+        backpackVolume -= things[i][1];
+        i++;
+
+        if (things[i] === undefined) {
+            return cost;
+        }
+    }
+
+    cost += backpackVolume / things[i][1] * things[i][0];
+    return cost;
+}
+
+/**
+ * IO section.
+ * Expected input example: '2 50\n60 20\n30 43'.
+ */
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (data) {
+    if (typeof data !== 'string') {
+        return;
+    }
+
+    const strings = data.split('\n');
+    const [thingsNumber, backpackVolume] = strings[0].split(' ').map(val => parseInt(val));
+    let things = [];
+    for (let i = 1; i <= thingsNumber; i++) {
+        things.push(strings[i].split(' ').map(val => parseInt(val)));
+    }
+
+    const cost = calculateMaxCost(backpackVolume, things);
+
+    process.stdout.write(cost.toFixed(3));
+});
